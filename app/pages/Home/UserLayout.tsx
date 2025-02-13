2901fbfdc933608fe83639a3b340ccf862924a37
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2025-02-13 16:43:30 +0600

    error fix

--- app/pages/Home/UserLayout.tsx
+++ app/pages/Home/UserLayout.tsx
@@ -1,16 +1,23 @@
-'use client'
+"use client";
 
 import Footer from "@/app/components/Footer";
 import Header from "@/app/components/Header";
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 import { ThemeProvider } from "next-themes";
+import { useEffect, useState } from "react";
 
 export default function RootLayout(props: { children: React.ReactNode }) {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null;
   return (
     <ChakraProvider value={defaultSystem}>
       <ThemeProvider attribute="class" disableTransitionOnChange>
         <Header></Header>
-
         <div>{props.children}</div>
         <Footer></Footer>
       </ThemeProvider>
