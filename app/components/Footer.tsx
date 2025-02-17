48fdcd10c7f20aeda4e2422eb35e15c432db036c
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2025-02-17 21:35:35 +0600

    added footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,28 +1,91 @@
-import React from 'react'
-
-export default function Footer() {
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import logo1 from "../../public/Images/logo1.png";
+import google from "../../public/Images/google.png";
+import app from "../../public/Images/app.png";
+const Footer = () => {
   return (
-    <div className='absolute bottom-0 w-full bg-darkBlack  h-40'>
-      <footer className="container mx-auto text-lightWhite   py-6 px-3">
-        <p className="text-sm">
-          If you have any questions or concerns, please feel free to contact us
-          at{" "}
-          <a
-            href="mailto:info@Ebstac.com"
-            className="text-yellow  font-semibold hover:underline"
-          >
-            info@Ebstac.com
-          </a>
-        </p>
-        <p className="text-sm mt-2">
-          Or{" "}
-          <strong className="font-bold text-lg">01312100288 (WhatsApp)</strong>.
-        </p>
+    <footer className="absolute bottom-0 w-full bg-yellow pb-6">
+      <div className="container mx-auto text-lightWhite   py-6 px-2 grid grid-cols-1 md:grid-cols-4 gap-4 md:gap-8">
+        <div>
+          <div className="flex items-center mb-2 md:mb-4">
+            <Image src={logo1} alt="Cookbd Logo" className="h-12 w-24"/>
+            <h2 className="text-darkBlack text-2xl font-bold">Cookbd</h2>
+          </div>
+          <p className="text-lightWhite text-sm">
+            If you have any questions or concerns, please feel free to contact
+            us at
+          </p>
+        </div>
 
-        <div className="border-t border-white opacity-50 my-4 w-full"></div>
+        <div>
+          <h3 className="text-lightWhite font-bold mb-1 md:mb-4">Company</h3>
+          <ul className="text-lightWhite md:space-y-2">
+            <li>
+              <Link href="#" className="hover:underline">
+                About us
+              </Link>
+            </li>
+            <li>
+              <Link href="#" className="hover:underline">
+                Blog
+              </Link>
+            </li>
+          </ul>
+        </div>
 
-        <p className="text-xs opacity-80">© 2024 Ebstac Skills Limited</p>
+        <div>
+          <h3 className="text-lightWhite font-bold mb-1 md:mb-4">Contact</h3>
+          <ul className="text-lightWhite md:space-y-2">
+            <li>
+              <Link href="#" className="hover:underline">
+                Help & Support
+              </Link>
+            </li>
+            <li>
+              <Link href="#" className="hover:underline">
+                Partner with us
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <h3 className="text-lightWhite font-bold mb-1 md:mb-4">Legal</h3>
+          <ul className="text-lightWhite md:space-y-2">
+            <li>
+              <Link href="#" className="hover:underline">
+                Terms & Conditions
+              </Link>
+            </li>
+            <li>
+              <Link href="#" className="hover:underline">
+                Refund & Cancellation
+              </Link>
+            </li>
+            <li>
+              <Link href="#" className="hover:underline">
+                Privacy Policy
+              </Link>
+            </li>
+            <li>
+              <Link href="#" className="hover:underline">
+                Cookie Policy
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="md:mt-10 text-center">
+        <h3 className="text-lightWhite font-bold mb-1 md:mb-4">Get in touch</h3>
+        <div className="flex justify-center gap-4">
+          <Image src={google} alt="Google Play" className="h-10 w-36" />
+          <Image src={app} alt="App Store" className="h-12 w-36"/>
+        </div>
+      </div>
     </footer>
-    </div>
-  )
-}
+  );
+};
+
+export default Footer;
