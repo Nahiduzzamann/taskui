3a4c55e49d8821660b359d18dd74faf08c8cda17
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2025-02-18 13:20:19 +0600

    fix responsive issue

--- app/[...notfound]/page.tsx
+++ app/[...notfound]/page.tsx
@@ -4,7 +4,7 @@ import { FaArrowLeft } from "react-icons/fa";
 
 const NotFoundPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-lightGray ">
+    <div className="flex flex-col items-center md:justify-center  md:h-screen h-[1000px] bg-lightGray pt-32 md:pt-0">
       <h1 className="text-7xl font-bold text-darkBlack ">404</h1>
       <h2 className="text-2xl font-semibold text-darkBlack  md:mt-4 mt-2">
         Page Not Found
